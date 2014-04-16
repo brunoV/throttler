56e14bef63eed365fc182debb515f833add4f815
Bruno Vecchi <vecchi.b@gmail.com>
2014-04-16 19:34:26 -0300

    Create page with API docs

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject throttler "0.1.5"
-  :description "Control the throughput of function calls and core.async channels using the token bucket algorithm"
-  :url "https://github.com/brunoV/throttler"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
-                                  [criterium "0.4.3"]]}})
