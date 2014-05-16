0cac8579cb00bbb26b84dc8ee3551a2423d7ac02
John Evans <lgastako@gmail.com>
2014-05-16 00:20:07 -0400

    Port to cljx.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject throttler "1.0.0"
+(defproject throttler "1.0.0-cljx"
   :description "Control the throughput of function calls and core.async channels using the token bucket algorithm"
   :url "https://github.com/brunoV/throttler"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
-                                  [criterium "0.4.3"]]}})
+                                  [criterium "0.4.3"]]}}
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :hooks [cljx.hooks]
+  :source-paths ["gen/src"]
+  :cljx {:builds [{:source-paths ["src"] :output-path "gen/src" :rules :clj}
+                  {:source-paths ["src"] :output-path "gen/src" :rules :cljs}]})
