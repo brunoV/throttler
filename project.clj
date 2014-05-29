b3a044bf1b56601fee5009a89e124943f9bdd742
Bruno Vecchi <vecchi.b@gmail.com>
2014-05-29 14:38:32 -0300

    Upgrade dependencies
    Clojure, core.async and Midje

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/brunoV/throttler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [criterium "0.4.3"]]}})
