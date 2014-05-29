2a076106de7b13e92cf950141d0192526ed6212a
Bruno Vecchi <vecchi.b@gmail.com>
2014-05-29 10:53:25 -0300

    Clean up bench helper

--- test/throttler/bench.clj
+++ test/throttler/bench.clj
@@ -1,13 +1,5 @@
 (ns throttler.bench
-  (:require [criterium.core :as c :refer [time-body]]
-            [throttler.core :as t :refer [throttle-chan]]
-            [clojure.core.async :as async :refer [<!! >!! chan]]))
-
-(def very-fast      (let [in (chan 1)] [in (throttle-chan in 10 :millisecond)]))
-(def fast           (let [in (chan 1)] [in (throttle-chan in 1 :millisecond)]))
-(def slow           (let [in (chan 1)] [in (throttle-chan in 100 :second)]))
-(def very-slow      (let [in (chan 1)] [in (throttle-chan in 10  :second)]))
-(def very-very-slow (let [in (chan 1)] [in (throttle-chan in 1 :second)]))
+  (:require [criterium.core :as c :refer [time-body]]))
 
 (defn rate [f expected-in-s]
   (let [n (* 2 expected-in-s)
@@ -15,7 +7,3 @@
         time-s (/ time-ns 1E9)
         rate (double (/ n time-s))]
     rate))
-
-(defn pipe-message [[in out]]
-    (>!! in :token)
-    (<!! out))
